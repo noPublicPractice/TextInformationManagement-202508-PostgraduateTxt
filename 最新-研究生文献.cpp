1e23f5f7c2895cb6a25f066c5747fbd988a4fca5
noPublicPractice <121429222+noPublicPractice@users.noreply.github.com>
2025-08-01 18:43:19 +0800

    Update 最新-研究生文献.cpp

--- 最新-研究生文献.cpp
+++ 最新-研究生文献.cpp
@@ -3,23 +3,23 @@
 	20250711分享：微信群下载 StatioCL: Contrastive Learning for Time Series via Non-Stationary and Temporal Contrast https://dl.acm.org/doi/10.1145/3627673.3679732
 	20250711分享：微信群下载 Intrapartum Fetal Risk Prediction with Apgar-Driven Deep Learning on Cardiotocography Signals
 	20250717分享：微信群下载 Denoising-Aware Contrastive Learning for Noisy Time Series https://arxiv.org/abs/2406.04627v1
-			拓展：arxiv.下载 参考Va-借鉴自回归模型的-Attention Is All You Need https://arxiv.org/abs/1706.03762v7
+		拓展：arxiv.下载 参考Va-借鉴自回归模型的-Attention Is All You Need https://arxiv.org/abs/1706.03762v7
 }
 扩散模型系列{
 	20250613分享：微信群下载 Diffusion models in vision: A survey
-			拓展：arxiv.下载 参考01-首次提出DDPM的-Deep unsupervised learning using nonequilibrium thermodynamics https://arxiv.org/abs/1503.03585v8
+		拓展：arxiv.下载 参考01-首次提出DDPM的-Deep unsupervised learning using nonequilibrium thermodynamics https://arxiv.org/abs/1503.03585v8
 	20250613分享：微信群下载 DIFF-FECG: A Conditional Diffusion-Based Method for Fetal ECG Extraction from Abdominal ECG
-			拓展：？？？下载 参考28-ADDB数据来源-PhysioBank, PhysioToolkit, and PhysioNet
-			拓展：nature下载 参考30-BDDB数据来源-Fetal electrocardiograms, direct and abdominal with reference ... https://www.nature.com/articles/s41597-020-0538-z
-			拓展：arxiv.下载 参考36-首次提出DDPM的-Denoising diffusion probabilistic models https://arxiv.org/abs/2006.11239v2
+		拓展：orgWeb下载 参考28-ADDB数据来源-PhysioBank, PhysioToolkit, and PhysioNet https://www.ahajournals.org/doi/pdf/10.1161/01.CIR.101.23.e215
+		拓展：nature下载 参考30-BDDB数据来源-Fetal electrocardiograms, direct and abdominal with reference ... https://www.nature.com/articles/s41597-020-0538-z
+		拓展：arxiv.下载 参考36-首次提出DDPM的-Denoising diffusion probabilistic models https://arxiv.org/abs/2006.11239v2
 	20250613分享：微信群下载 Diffuse and Disperse: Image Generation with Representation Regularization https://arxiv.org/abs/2506.09027
 	20250711分享：微信群下载 DENOISING DIFFUSION IMPLICIT MODELS https://arxiv.org/abs/2010.02502v4
 	20250727分享：arxiv.下载 A Survey on Diffusion Models for Time Series and Spatio-Temporal Data https://arxiv.org/abs/2404.18886
 }
 物理信息化机器学习系列{
-	20250507分享：？？？下载 Physics-informed machine learning
+	20250507分享：govWeb下载 Physics-informed machine learning https://www.osti.gov/pages/servlets/purl/2282016
 	20250610分享：arxiv.下载 Physics-informed deep learning for infectious disease forecasting https://arxiv.org/abs/2501.09298v1
-	20250610分享：？？？下载 surrogate modeling for nonplanar wavefront excitation of topological nanophotonic devices
+	20250610分享：arxiv.下载 Active Physics-Informed Deep Learning Surrogate Modeling for Non Planar Wavefront Excitation ... https://arxiv.org/abs/2410.16905
 	20250610分享：arxiv.下载 Scalable physics-informed deep generative model for solving forward and inverse stochastic ... https://arxiv.org/abs/2503.18012v1
 	20250610分享：arxiv.下载 A Physics-Informed Deep Learning Model for MRI Brain Motion Correction https://arxiv.org/abs/2502.09296v1
 	20250623分享：arxiv.下载 DeePoly: A High-Order Accuracy Scientific Machine Learning Framework for Function Approximation ... https://arxiv.org/abs/2506.04613v3
@@ -28,18 +28,23 @@
 }
 未分类{
 	20250508分享：微信群下载 Contractile Activity Detection via Contrastive Learning in Electrohysterography Based Perinatal Care
-	20250509分享：？？？下载 Motor learning refines thalamic influence on motor cortex
+	20250509分享：nature下载 Motor learning refines thalamic influence on motor cortex https://tyelab.org/wp-content/uploads/ramot_taschbach_2025.pdf
 	20250616分享：arxiv.下载 Graph Neural Network in Network Neuroscience https://arxiv.org/abs/2106.03535v2
-	20250623分享：nature下载 Unsupervised pretraining in biological neural networks https://doi.org/10.1038/s41586-025-09180-y
+	20250623分享：nature下载 Unsupervised pretraining in biological neural networks https://www.nature.com/articles/s41586-025-09180-y
 	20250623分享：arxiv.下载 A multimodal vision foundation model for clinical dermatology https://arxiv.org/abs/2410.15038
-	20250623分享：QUBweb下载 A deep learning system ... https://pure.qub.ac.uk/files/642367133/Jiang_DL_DeepStroke_Silent_and_Clinical_Stroke_Nature_BME_2025.pdf
+	20250623分享：nature下载 A deep learning system ... https://pure.qub.ac.uk/files/642367133/Jiang_DL_DeepStroke_Silent_and_Clinical_Stroke_Nature_BME_2025.pdf
 	20250623分享：arxiv.下载 Advancing Real-time Pandemic Forecasting Using Large Language Models: A COVID-19 Case Study https://arxiv.org/abs/2404.06962
 	20250716分享：微信群下载 A Two-Stage Noise-Tolerant Paradigm for Label Corrupted Person Re-Identification
 	20250716分享：微信群下载 Intrapartum electronic fetal heart rate monitoring to predict acidemia ... https://www.ajog.org/article/S0002-9378(24)00528-3/abstract
 	20250720分享：微信群下载 Mean Flows for One-step Generative Modeling https://arxiv.org/abs/2505.13447
-	20250727分享：群重复下载 Intrapartum electronic fetal ...
+	20250727分享：群重复分享 Intrapartum electronic fetal ...
 	20250727分享：工作群下载 Electrohysterography in the Diagnosis of Preterm Birth: a Review
 	20250727分享：工作群下载 Identification of contractions from Electrohysterography for prediction of prolonged labor
+##		拓展：arxiv.下载 Generative Adversarial Networks https://arxiv.org/abs/1406.2661
+		拓展：arxiv.下载 Unsupervised Representation Learning with Deep Convolutional Generative Adversarial Networks https://arxiv.org/abs/1511.06434
+##		拓展：arxiv.下载 Generative Adversarial Networks in Computer Vision: A Survey and Taxonomy https://arxiv.org/abs/1906.01529
+##	20250728分享：orgWeb下载 Evidential Neighborhood Contrastive Learning for Universal Domain ... https://ojs.aaai.org/index.php/AAAI/article/download/20575/20334
+##	20250729分享：群重复分享 Diffuse and Disperse: Image Generation with Representation Regularization https://arxiv.org/abs/2506.09027
 }
 未下载{
 	20250509分享：未下载     Department of Electrical Engineering
